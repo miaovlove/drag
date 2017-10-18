d1dbb2fb394fa81929141c86dceb355b7d15d584
usernamelove <1811930696@qq.com>
2017-10-18 12:24:06 +0800

    drag success

--- 2.js
+++ 2.js
@@ -1,9 +1,35 @@
 // JavaScript Document
-function drag(obj){
-	var dix=0;
-	var diy=0;
-	 
+window.onload = function(){
 	
-	
+	var oDiv = document.querySelector(".div1");
+	drag(oDiv);
 	
 	}
+	
+	function drag(obj){
+		
+		var dix=0;
+		var diy=0;
+		obj.onmousedown = function(ev){
+			var ev = ev||window.event;
+			
+			dix= ev.clientX-dix;
+			
+			diy=ev.clientY-diy;
+			
+			document.onmousemove = function(ev){
+				var ev = ev||window.event;
+				
+				obj.style.left=ev.clientX-dix+"px";
+				
+				obj.style.top= ev.clientY-diy+"px";
+				}
+				document.onmouseup = function(){
+					
+					document.onmousemove = null;
+					
+					document.onmouseup = null;}
+					return false;
+			}
+		
+		}
